57adc0f790976298f6d7b076dd38f1a6f80aeefb
yichiang <yi_chiang@outlook.com>
2016-07-23 07:47:01 -0700

    finish code

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+function bouncer(arr) {
+  var result=[];
+  for (var i = 0; i < arr.length; i++) {
+    arr[i]
+  }
+  // Don't show a false ID to this bouncer.
+  return arr;
+}
+
+bouncer([7, "ate", "", false, 9]);
